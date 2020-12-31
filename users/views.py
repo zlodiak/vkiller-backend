1abb026e5a1d0bf4ed46c9f9fb54b3a6a0d0894f
kalinin <prozaik81-2@yandex.ru>
2020-12-31 21:01:46 +0300

    aadd route for userslist

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http import JsonResponse
+from django.core import serializers
+import json
 
-# Create your views here.
+from .models import User
+
+def users(request):
+    print(12333)
+    users = User.objects.all()   
+    users_serialized = serializers.serialize('json', users)
+    return JsonResponse(users_serialized, safe=False)   
