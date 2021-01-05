2de5845c7751f28338674113641e23dd6a70f649
kalinin <prozaik81-2@yandex.ru>
2021-01-05 17:55:22 +0300

    comment code for victims get

--- victims/views.py
+++ victims/views.py
@@ -11,3 +11,15 @@ def victims(request):
     victims = Victim.objects.all()   
     victims_serialized = serializers.serialize('json', victims)
     return JsonResponse(victims_serialized, safe=False)
+
+# def victims(request):
+#     print('=-=-=', request.GET)
+#     # if request.method == 'GET':
+        
+
+#         # result = request.body.decode("utf-8")
+#         # user_id = json.loads(result).get('user_id')
+        
+#         # victims = Victim.objects.filter(id_user=user_id) 
+
+#     return JsonResponse({'isLogged': isLogged})
